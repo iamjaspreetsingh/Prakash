fc246ed57a56b6955851bba366312ae42ecaee02
iamjaspreetsingh <jsk1961998@gmail.com>
2017-10-28 02:55:39 +0530

    js

---
+++ app/src/main/java/com/jskgmail/lifesaver/Main2Activity.java
@@ -0,0 +1,52 @@
+package com.jskgmail.lifesaver;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        go();
+    }
+
+
+
+
+    void go(){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+final String TAG="qqqq";
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/jskgmail/lifesaver/Main3Activity.java
@@ -0,0 +1,61 @@
+package com.jskgmail.lifesaver;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.VideoView;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        go();
+    }
+    private void go() {
+
+        VideoView v=(VideoView)findViewById(R.id.videoView2) ;
+
+String s="https://firebasestorage.googleapis.com/v0/b/lifesaver-18f28.appspot.com/o/earthquake.mp4?alt=media&token=98993371-823d-4b81-adc7-d9d87ec841a9";
+        Uri uri=Uri.parse(s);
+        v.setVideoURI(uri);
+         v.setVisibility(View.VISIBLE);
+         v.setVideoPath(s);
+        v.requestFocus();
+        v.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
