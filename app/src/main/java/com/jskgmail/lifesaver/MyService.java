40a5b238052e6bf0490ffde14a52f85e3a1df25a
iamjaspreetsingh <jsk1961998@gmail.com>
2017-12-04 00:17:26 +0530

    background service

--- app/src/main/java/com/jskgmail/lifesaver/MyService.java
+++ app/src/main/java/com/jskgmail/lifesaver/MyService.java
@@ -1,8 +1,13 @@
 package com.jskgmail.lifesaver;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
@@ -31,9 +36,13 @@ import retrofit2.http.GET;
  * Created by TutorialsPoint7 on 8/23/2016.
  */
 
-public class MyService extends Service {
+public class MyService extends Service implements SensorEventListener {
 
-
+    private Sensor senAccelerometer;
+    private Sensor TemperatureSensor;
+    private SensorManager senSensorManager;
+    float lastx=0,lasty=0,lastz=0;
+    long lastupdate = System.currentTimeMillis();
 
     @Nullable
     @Override
@@ -52,6 +61,25 @@ public MyService()
 
 
 final String TAG="service";
+
+
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        TemperatureSensor = senSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        senSensorManager.registerListener(this,
+                TemperatureSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
+
+
+
+
+
+
 
 
        ApiInterfaceearthquake apiServiceearth = ApiClientearthquake.getClient().create(ApiInterfaceearthquake.class);
@@ -78,7 +106,8 @@ final String TAG="service";
         });
 
 
-if(MainActivity.flood.equals("1"))
+
+        if(MainActivity.flood.equals("1"))
 {    Intent i=new Intent(MyService.this,MainActivity.class);
         startActivity(i);
 onDestroy();}
@@ -324,6 +353,104 @@ public class Earthquakefinder extends AsyncTask<String, Void, Integer> {
         return result;
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            long diffTime = (curTime - lastupdate);
+
+            if ((curTime - lastupdate) > 100) {
+
+
+
+                float speed = Math.abs(x + y + z - lastx - lasty - lastz)/ diffTime * 10000;
+                Log.v("speeed",speed+"" );
+                if (speed > 4000) {
+//TODO alert
+                    MainActivity.flood = "1";
+                    SharedPreferences.Editor editor = getSharedPreferences("flood", MODE_PRIVATE).edit();
+                    editor.putString("flood", "1");
+                    editor.apply();
+                    Intent intent=new Intent(MyService.this,MainalertActivity.class);
+                    startActivity(intent);
+                }
+
+                lastx = x;
+                lasty = y;
+                lastz = z;
+
+
+
+
+                lastupdate = curTime;
+            }
+        }
+
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE){
+            Log.e("temp",sensorEvent.values[0]+"");
+            if (sensorEvent.values[0]>50){ MainActivity.flood = "1";
+                SharedPreferences.Editor editor = getSharedPreferences("flood", MODE_PRIVATE).edit();
+                editor.putString("flood", "1");
+                editor.apply();}
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.e("accuracyyy",""+accuracy);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
