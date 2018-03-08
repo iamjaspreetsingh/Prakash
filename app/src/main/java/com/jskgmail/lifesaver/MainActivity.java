401107abb72c10982bce5ba267e8cd28d90a2e4e
iamjaspreetsingh <jsk1961998@gmail.com>
2018-03-08 07:29:58 +0530

    bt

--- app/src/main/java/com/jskgmail/lifesaver/MainActivity.java
+++ app/src/main/java/com/jskgmail/lifesaver/MainActivity.java
@@ -15,13 +15,17 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -61,13 +65,16 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import okhttp3.ResponseBody;
@@ -79,6 +86,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity
@@ -106,14 +114,14 @@ static String hospp="No Hospital Found";
     double eleva = 0;
     static String latitude="28.620660,77.08127";
    static double diffelevation = 0;
-
+    File image = null;
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLocation;
     private LocationManager locationManager;
     private LocationRequest mLocationRequest;
 
-
+    String mCurrentPhotoPath;
     private Sensor senAccelerometer;
     private SensorManager senSensorManager;
 float lastx=0,lasty=0,lastz=0;
@@ -2002,6 +2010,53 @@ String username="",name="",myno="98";
 
 
 
+
+    public static class ApiClientdead {
+
+        public static final String BASE_URL ="https://api-us.faceplusplus.com/facepp/v3/";
+
+        private static Retrofit retrofit = null;
+
+        public static Retrofit getClient() {
+            if (retrofit==null) {
+                retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+            }
+            return retrofit;
+        }
+
+    }
+    public interface ApiInterfacedead {
+
+        @POST("search")
+        retrofit2.Call<ResponseBody> getall(@Query("api_key") String code,
+                                            @Query("api_secret") String monthact,
+                                            // @Query("image_file") File file,
+                                            @Query("image_url") String url,
+
+                                            @Query("faceset_token") String faceset
+        );
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static class ApiClienthospital {
 
         public static final String BASE_URL ="https://data.gov.in/node/356921/datastore/export/";
@@ -2503,6 +2558,10 @@ Log.e("MainActivity", "Failed to pick contact");
             startActivity(i);
 
         }
+        else if (id==R.id.blue)
+        {
+
+        }
         else if (id==R.id.testmy)
         { MainActivity.flood="1";
             SharedPreferences.Editor editor=getSharedPreferences("flood",MODE_PRIVATE).edit();
@@ -2511,6 +2570,12 @@ Log.e("MainActivity", "Failed to pick contact");
             editor.apply();
 Toast.makeText(getApplicationContext(),"It is a fake test of how app works during any mishap",Toast.LENGTH_LONG).show();
 
+        }
+        else if (id==R.id.dead)
+        {
+            dispatchTakePictureIntent();
+
+            recogniseface();
         }
         else if (id==R.id.about)
         {
@@ -2557,6 +2622,56 @@ Toast.makeText(getApplicationContext(),"It is a fake test of how app works durin
         return true;
     }
 
+
+
+    private File createImageFile(){
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            goapii();
+        }
+        return image;
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            photoFile = createImageFile();
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.jskgmail.lifesaver.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+
+            }
+
+        }
+    }
+
+
+
+
     private void contactPicked(Intent data) {
         ListView l=(ListView)findViewById(R.id.lv);
         Cursor cursor = null;
@@ -2601,6 +2716,261 @@ phon=phon+phoneNo+",";
 
 
 
+
+    void goapidead()
+    {
+
+
+
+
+        ApiInterfacedead apiService1 = ApiClientdead.getClient().create(ApiInterfacedead.class);
+       //TODO
+        retrofit2.Call call = apiService1.getall("8eXIfwPbVhLUXV4xt9eW2xRSxWt74Fki","9xyBX7iWUUWu4msZbaAm6_XTRN9OiT5b","https://firebasestorage.googleapis.com/v0/b/attendance-4e350.appspot.com/o/06112011292-001.jpg?alt=media&token=a087ec2b-891c-4182-8df7-1917685aeb11","537c2b49a9a160655b9a3c707555af4b");
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                Log.e(TAG, "success");
+                Log.e(TAG, response.raw().request().url().toString());
+                String url = response.raw().request().url().toString();
+                Log.e(TAG, url);
+                Dead mytask = new Dead();
+                mytask.execute(url);
+
+
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Log.e(TAG, "failureee");
+            }
+
+
+        });
+
+
+
+
+    }
+
+    private class Dead extends AsyncTask<String, Void, Integer> {
+
+
+
+        public Dead() {
+
+            super();
+
+        }
+
+
+        // The onPreExecute is executed on the main UI thread before background processing is
+
+        // started. In this method, we start the progressdialog.
+
+        @Override
+
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+
+            // Show the progress dialog on the screen
+
+
+        }
+
+
+        // This method is executed in the background and will return a result to onPostExecute
+
+        // method. It receives the file name as input parameter.
+
+        @Override
+
+        protected Integer doInBackground(String... urls) {
+
+
+            InputStream inputStream = null;
+
+            HttpURLConnection urlConnection = null;
+
+            Integer result = 0;
+
+
+            // TODO connect to server, download and process the JSON string
+
+
+            // Now we read the file, line by line and construct the
+
+            // Json string from the information read in.
+
+            try {
+
+                /* forming th java.net.URL object */
+
+                URL url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+
+
+                 /* optional request header */
+
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+
+
+
+                /* optional request header */
+
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+
+
+                /* for Get request */
+
+                urlConnection.setRequestMethod("POST");
+
+                int statusCode = urlConnection.getResponseCode();
+
+
+
+                /* 200 represents HTTP OK */
+
+                if (statusCode == 200) {
+
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+
+
+                    // Convert the read in information to a Json string
+
+                    String response = convertInputStreamToString(inputStream);
+
+
+                    LayoutInflater inflater = getLayoutInflater();
+                    View alertLayout = inflater.inflate(R.layout.layoutdead, null);
+                  AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    TextView t=(TextView)alertLayout.findViewById(R.id.textView82);
+                    t.setText(response);
+
+                    // this is set the view from XML inside AlertDialog
+                    alert.setView(alertLayout);
+                    // disallow cancel of AlertDialog on click of back button and outside touch
+                    alert.setTitle("Identify Dead person ");
+                    alert.setIcon(R.drawable.ic_cast_grey);
+                    alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+
+                    alert.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+
+                        }
+                    });
+                    AlertDialog dialog = alert.create();
+                    dialog.show();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    result = 1; // Successful
+
+                } else {
+
+                    result = 0; //"Failed to fetch data!";
+
+                }
+
+            } catch (Exception e) {
+
+                Log.d(TAG, e.getLocalizedMessage());
+
+            }
+
+            return result; //"Failed to fetch data!";
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void recogniseface() {
+
+
+
+
+goapidead();
+
+
+    }
 
 
 
